a41c58c9f99981607d3fd7652ce291825ae71b91
Cameron Pittman <cameron.w.pittman@gmail.com>
2020-02-12 17:36:41 -0500

    update planned tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const wasm = require("../pkg");
-const { STN } = wasm;
+const { Plan, STN } = wasm;
 
 // taken from MIT 16.412 L02 (see docs/references/)
 const example1 = {
@@ -17,24 +17,90 @@ describe("temporal-networks", () => {
     expect(wasm).to.be.ok;
   });
 
+  describe("Plan", () => {
+    it("should have a root", () => {
+      const plan = new Plan();
+
+      expect(plan.getRoot()).to.be.a("Step");
+    });
+
+    it('should be able to create a step', () => {
+      const plan = new Plan();
+      const step = plan.createStep(duration=[0, 1]);
+      expect(step).to.be.ok;
+      expect(step.interval).to.equal([0, 1]);
+      expect(step.start).to.equal(0);
+    });
+
+    it("should give high level statistics", () => {
+      const plan = new Plan();
+
+      expect(plan.getDuration()).to.equal([0, 0]);
+    })
+
+    it("should compose steps", () => {
+      // no duration given to the plan, so no overall plan constraint
+      const plan = new Plan();
+
+      // implicitly put the step on the plan root
+      const step1 = plan.createStep(duration=[0, 1]);
+      plan.createStep(follows=step1, duration=[5, 10]);
+
+      // summed durations
+      expect(plan.getDuration()).to.equal([5, 11]);
+    })
+
+    it("should allow new steps to precede old ones", () => {
+      // no duration given to the plan, so no overall plan constraint
+      const plan = new Plan();
+
+      // implicitly put the step on the plan root
+      const step1 = plan.createStep(duration=[0, 1]);
+      // insert this step between the root and step 1
+      plan.createStep(precedes=step1, duration=[5, 10]);
+
+      // summed durations
+      expect(plan.getDuration()).to.equal([5, 11]);
+    })
+
+    it("should allow parallel steps", () => {
+      const plan = new Plan();
+
+      // two steps in parallel; both start at the plan root
+      plan.createStep(follows=plan.getRoot(), duration=[4, 9]);
+      plan.createStep(follows=plan.getRoot(), duration=[5, 10]);
+
+      // union the intervals
+      expect(plan.getDuration()).to.equal([5, 9]);
+    })
+
+    it("should compute plan durations with overall constraints", () => {
+      // put a constraint on the root
+      const plan = new Plan(duration=[8, 20]);
+
+      const step1 = plan.createStep(duration=[0, 1]);
+      plan.createStep(follows=step1, duration=[5, 10]);
+      plan.createStep(precedes=step1, duration=[4, 8]);
+
+      // union summed step duration with the plan's duration
+      expect(plan.getDuration()).to.equal([9, 19]);
+    })
+
+    describe('Step', () => {
+      it('should register an onChange callback', () => {
+        const plan = new Plan();
+      })
+    })
+  })
+
   describe("STN", () => {
     it("should create a graph from a set of edges", () => {
       const stn = new STN();
-      const res = stn.initialize(example1, {
+      const [numNodes, numEdges] = stn.initialize(example1, {
         implicit_intervals: false
       });
-      const [numNodes, numEdges] = res;
       expect(numNodes).to.equal(4);
-      expect(numEdges).to.equal(8);
-    });
-
-    it("should have the correct implicit intervals", () => {
-      const stn = new STN();
-      stn.initialize(example1, {
-        implicit_intervals: false
-      });
-      console.log(stn.dumpConstraintTable());
-      expect(true).to.be.false;
+      expect(numEdges).to.equal(12);
     });
   });
 });
