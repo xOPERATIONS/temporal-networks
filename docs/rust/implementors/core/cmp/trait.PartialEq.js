1e92ff71be2b49c02655e309ee255d9b21b47765
Cameron Pittman <cameron.w.pittman@gmail.com>
2020-03-18 17:37:51 -0400

    update docs

--- docs/rust/implementors/core/cmp/trait.PartialEq.js
+++ docs/rust/implementors/core/cmp/trait.PartialEq.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["temporal_networks"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>&gt; for <a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>",synthetic:false,types:["temporal_networks::interval::Interval"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["temporal_networks"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>&gt; for <a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>","synthetic":false,"types":["temporal_networks::interval::Interval"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"struct\" href=\"temporal_networks/schedule/struct.Episode.html\" title=\"struct temporal_networks::schedule::Episode\">Episode</a>&gt; for <a class=\"struct\" href=\"temporal_networks/schedule/struct.Episode.html\" title=\"struct temporal_networks::schedule::Episode\">Episode</a>","synthetic":false,"types":["temporal_networks::schedule::Episode"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/rust/implementors/serde/ser/trait.Serialize.js
+++ docs/rust/implementors/serde/ser/trait.Serialize.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["temporal_networks"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>",synthetic:false,types:["temporal_networks::interval::Interval"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["temporal_networks"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"temporal_networks/interval/struct.Interval.html\" title=\"struct temporal_networks::interval::Interval\">Interval</a>","synthetic":false,"types":["temporal_networks::interval::Interval"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
